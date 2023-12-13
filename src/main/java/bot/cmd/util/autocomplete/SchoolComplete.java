afd5b6d403d1b20c37b077847dbde838e6b110b9
Mooner510 <lmck510@gmail.com>
2023-12-13 15:28:20 +0900

    optimized school

--- src/main/java/bot/cmd/util/autocomplete/SchoolComplete.java
+++ src/main/java/bot/cmd/util/autocomplete/SchoolComplete.java
@@ -9,13 +9,10 @@ import static bot.Main.schools;
 
 public class SchoolComplete {
     public static void schoolComplete(CommandAutoCompleteInteractionEvent event) {
-        HashSet<String> strings = new HashSet<>(schools.keySet());
-        strings.removeIf(s -> !s.startsWith(event.getFocusedOption().getValue()));
-        HashSet<Command.Choice> choices = new HashSet<>();
-        for (String s : strings) {
-            if (choices.size() >= 24) break;
-            choices.add(new Command.Choice(s, s));
-        }
-        event.replyChoices(choices).queue();
+        event.replyChoices(schools.keySet().parallelStream()
+                .filter(s -> s.startsWith(event.getFocusedOption().getValue()))
+                .map(s -> new Command.Choice(s, s))
+                .limit(24)
+                .toList()).queue();
     }
 }
